12e9e6bfd0b3bb7641474b093f46d82e64aa9d3a
Mohamed <mohamed.khelif@mongodb.com>
2020-10-27 22:39:53 -0400

    test time

--- clock.py
+++ clock.py
@@ -1,25 +1,28 @@
 import os
 import time
-from lib.waveshare_epd import epd4in2bc
-from PIL import Image, ImageDraw, ImageFont
-resdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'resources')
-roboto = os.path.join(resdir, 'Roboto-Regular.ttf')
+from display import Display
+
 
 def main():
-	try:
-	# Display init, clear
-		display = epd4in2bc.EPD()
-		display.init()
-		display.Clear() # 0: Black, 255: White
-		w = display.height
-		h = display.width
-		print("Fetching font")
-		font = ImageFont.truetype(roboto, 24)
-		print('width:', w)
-		print('height:', h)
-	    ### ... IMAGE CODE ... ###
-	except IOError as e:
-    		print(e)
+    try:
+        d = Display()
+
+        while(True):
+            d.text((50, 100), getReadableTime())
+            d.text((50, 130), getReadableDate())
+            time.sleep(30)
+            d.clear()
+    except IOError as e:
+        print(e)
+
+
+def getReadableTime():
+    return time.strftime('%l:%M%p %z')
+
+
+def getReadableDate():
+    return time.strftime('%b %d, %Y')
+
 
 if __name__ == "__main__":
     main()
--- display.py
+++ display.py
@@ -1,34 +1,45 @@
 from lib.waveshare_epd import epd4in2
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image, ImageDraw, ImageFont
 import time
 import os
 
-imgdir = os.path.join('resources','img')
+imgdir = os.path.join('resources', 'img')
+
 
 class Display:
-	epd = None
-	HBlackimage = None
-	drawblack = None
-	robotoFont = ImageFont.truetype(os.path.join('resources', 'roboto.ttf'), 24)
-	selectedFont = robotoFont
-	def __init__(self):
-		try:
-			self.epd = epd4in2.EPD()
-			self.epd.init()
-			self.epd.Clear()
-			self.HBlackimage = Image.new('1', (self.epd.width, self.epd.height), 255)
-			self.drawblack = ImageDraw.Draw(self.HBlackimage)
-		except IOError as e:
-			print(e)
-	def text(self,coord, str):
-		self.drawblack.text(coord, str, font = self.robotoFont)
-	def image(self, imgstr, coord = (0,0), dimensions = (100,100)):
-		Himg = Image.new('1', dimensions, 255)
-		jpg = Image.open(os.path.join(imgdir, imgstr))
-		Himg.paste(jpg, coord)
-		self.epd.display(self.epd.getbuffer(Himg))
-	def draw(self):
-		self.epd.display(self.epd.getbuffer(self.HBlackimage))
-		time.sleep(2)
-	def clear(self):
-		self.epd.Clear()
+    epd = None
+    HBlackimage = None
+    drawblack = None
+    robotoFont = ImageFont.truetype(
+        os.path.join('resources', 'roboto.ttf'), 24)
+    selectedFont = robotoFont
+
+    def __init__(self):
+        try:
+            self.epd = epd4in2.EPD()
+            self.epd.init()
+            self.epd.Clear()
+            self.HBlackimage = Image.new(
+                '1', (self.epd.width, self.epd.height), 255)
+            self.drawblack = ImageDraw.Draw(self.HBlackimage)
+        except IOError as e:
+            print(e)
+
+    def text(self, coord, str):
+        self.drawblack.text(coord, str, font=self.robotoFont)
+
+    def image(self, imgstr, coord=(0, 0), dimensions=(100, 100)):
+        Himg = Image.new('1', dimensions, 255)
+        jpg = Image.open(os.path.join(imgdir, imgstr))
+        Himg.paste(jpg, coord)
+        self.epd.display(self.epd.getbuffer(Himg))
+
+    def draw(self):
+        self.epd.display(self.epd.getbuffer(self.HBlackimage))
+        time.sleep(2)
+
+    def clear(self):
+        self.HBlackimage = Image.new(
+            '1', (self.epd.width, self.epd.height), 255)
+        self.drawblack = ImageDraw.Draw(self.HBlackimage)
+        self.epd.Clear()
